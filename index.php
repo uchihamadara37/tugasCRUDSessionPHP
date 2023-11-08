290b761a2d4ec9ffabbb13e130c63c8bb19a5e8f
uchihamadara37 <uchihaandreashisui@gmail.com>
2023-11-08 12:56:29 +0700

    Menambah

--- index.php
+++ index.php
@@ -9,6 +9,17 @@
 
 </head>
 <body>
+    <?php 
+        $koneksi = mysqli_connect("localhost","root","","loginpraktikum");
+        if (mysqli_connect_errno()){
+            echo "Koneksi database gagal : " . mysqli_connect_error();
+        }
+
+        
+		
+
+    ?>
+
     <nav class="navbar navbar-expand-lg navbar-dark">
         <a class="navbar-brand" href="#">Navbar</a>
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
@@ -32,6 +43,80 @@
         </div>
     </nav>
 
+   
+
+    <div class="row my-5">
+        <div class="col-1"></div>
+        <div class="col-2">
+            <div class=" text-center">
+                <button type="button" class="btn btn-primary " >Sign Up (Create)</button>
+            </div>
+        </div>
+        <div class="col-2">
+            <div class=" text-center">
+                <button type="submit" class="btn btn-info">Sign In (Create)</button>
+            </div>
+            
+        </div>
+        <div class="col-2">
+            <div class=" text-center">
+                <button type="submit" class="btn btn-secondary">All Data (Read)</button>
+            </div>
+            
+        </div>
+        <div class="col-2">
+            <div class=" text-center">
+                <button type="submit" class="btn btn-success">Update Data (Update)</button>
+            </div>
+            
+        </div>
+        <div class="col-2">
+            <div class=" text-center">
+                <button type="submit" class="btn btn-danger">Delete Data (Delete)</button>
+            </div>
+            
+        </div>
+        <div class="col-1"></div>
+    </div>
+
+    <div class="row ">
+        <div class="col-2"></div>
+        <div class="col-8 kotak2">
+            <table class="table table-bordered">
+                <tr>
+                    <th>No.</th>
+                    <th>Nama</th>
+                    <th>Username</th>
+                    <th>Email</th>
+                    <th>Password</th>
+                </tr>
+                
+
+                <?php 
+                    $no = 1;
+                    $data = mysqli_query($koneksi,"select * from user");
+                    while($d = mysqli_fetch_array($data)){
+                ?>
+                        <tr>
+                            <td><?php echo $no++; ?></td>
+                            <td><?php echo $d['username']; ?></td>
+                            <td><?php echo $d['password']; ?></td>
+                            <td><?php echo $d['email']; ?></td>
+                            <td>
+                                <a href="edit.php?id=<?php echo $d['id']; ?>">EDIT</a>
+                                <a href="hapus.php?id=<?php echo $d['id']; ?>">HAPUS</a>
+                            </td>
+                        </tr>
+                <?php 
+                    }
+                ?>
+
+                
+            </table>
+        </div>
+        <div class="col-2"></div>
+    </div>
+
     <form>
         <div class="row">
             <div class="col-4"></div>
@@ -39,10 +124,10 @@
                 <h4 class="text-center form-group">Sign Up</h4>
                 <div class="row form-group">
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="First name">
+                        <input type="text" class="form-control" placeholder="First name" id="val1">
                     </div>
                     <div class="col">
-                        <input type="text" class="form-control" placeholder="Last name">
+                        <input type="text" class="form-control" placeholder="Last name" id="val2">
                     </div>
                 </div>
                 <div class="row form-group">
@@ -62,12 +147,13 @@
                 </div>
                 <div class="row form-group">
                     <div class="col">
-                        <input type="password" class="form-control" placeholder="Repeat-password">
+                        <input type="password" class="form-control" placeholder="Repeat-password" >
                     </div>
                 </div>
                 <div class=" text-center">
-                    <button type="submit" class="btn btn-orange">Sign Up</button>
+                    <button type="submit" class="btn btn-orange" id="">Sign Up</button>
                 </div>
+                
             </div>
             <div class="col-4"></div>
         </div>
@@ -102,5 +188,9 @@
     <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+
+    <script>
+        
+    </script>
 </body>
 </html>
