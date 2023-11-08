f1bc8d8f0e229bbda78a653a5855d26b56601a0a
uchihamadara37 <uchihaandreashisui@gmail.com>
2023-11-08 10:38:09 +0700

    menambahkan gambar

--- index.php
+++ index.php
@@ -9,10 +9,31 @@
 
 </head>
 <body>
-    <h1>Hello world</h1>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <a class="navbar-brand" href="#">Navbar</a>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+        <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Home</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Features</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Pricing</a>
+                </li>
+                <li class="nav-item active">
+                    <a class="nav-link" href="#">Disabled</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
     <div class="row">
         <div class="col-4"></div>
-        <div class="col-4 .kotak">
+        <div class="col-4 kotak">
             <h4 class="text-center form-group">Sign Up</h4>
             <div class="row form-group">
                 <div class="col">
